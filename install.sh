d43a26ec6d30783a7b0ac6f477c922a0033f644a
Alexander Potts <alexander.potts@gmail.com>
2019-06-08 18:05:09 -0600

    Move .gitignore remove lines from inputrc modify install script

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ git clone https://github.com/
 cd dotfiles
 cp -r .dotfiles/ ~
 cd ~/.dotfiles/
-ln -sv .bash_profile ~
-ln -sv .intputrc ~
-ln -sv .gitignore ~
+cp .bash_profile ~
+cp .gitignore ~
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
