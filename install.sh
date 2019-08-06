5f6ab8ac7af2af4cdf9e0578479510093d95f236
Alexander Potts <alexander.potts@gmail.com>
2019-08-06 15:38:58 -0600

    more changes

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
+# Define colors
+RED="$(tput setaf 1)"
+NORMAL="$(tput sgr0)"
+
+colorEcho () {
+    printf "${RED} $1${NORMAL}\n"
+}
+
+colorEcho "Downloading git autocompletion."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-ln -s "$(realpath dotfiles)" ~/.dotfiles
-for FILE in `find ~/.dotfiles -maxdepth 1 -type f`; do
-	ln -s `realpath $FILE` $HOME
+colorEcho "Installing git-so-diff"
+source setup/git-so-diff-install.sh
+
+colorEcho "Cloning dotfiles..."
+git clone https://github.com/jakkso/dotfiles.git
+
+colorEcho "Setting up dotfiles"
+cd dotfiles
+for FILE in $(ls dotfiles/globals/); do
+	rm -rf ~/.$FILE
+	ln -s `realpath $FILE` ~/.$FILE
 done
 
-
 if [[ $(uname -s) == Darwin ]]; then
+	colorEcho "Setting up Mac"
 	source install-mac.sh
 fi
