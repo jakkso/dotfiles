bb7004f5723eef33a41932e3896f588b919803f1
Alexander Potts <alexander@jakkso.com>
2020-01-08 15:13:16 -0700

    increment python version to 3.8.0 Install linux libs earlier

--- install.sh
+++ install.sh
@@ -8,6 +8,15 @@ colorEcho () {
     printf "${RED} $1${NORMAL}\n"
 }
 
+if [[ $(uname -s) == Linux ]]; then
+  colorEcho "Installing essential libs"
+  sudo apt install build-essential npm nodejs bzip2 libbz2-dev \
+    libreadline6 libreadline6-dev libffi-dev libssl1.0-dev \
+    sqlite3 libsqlite3-dev git curl -Y
+  npm install -g diff-so-fancy
+  curl https://pyenv.run | bash
+fi
+
 colorEcho "Creating ~/bin"
 mkdir $HOME/bin/ &> /dev/null
 cd $HOME/bin/
@@ -16,26 +25,25 @@ colorEcho "Cloning dotfiles..."
 git clone https://github.com/jakkso/dotfiles.git
 cd dotfiles
 
-colorEcho "Installing git-so-diff"
+colorEcho "Installing git-so-diff..."
 source setup/git-so-diff-install.sh
 
-colorEcho "Downloading git autocompletion."
+colorEcho "Downloading git autocompletion..."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-colorEcho "Setting up dotfiles"
+colorEcho "Setting up dotfiles..."
 cd dotfiles/globals
 for FILE in $(ls); do
     rm -rf ~/.${FILE}
     ln -s $(realpath ${FILE}) ~/.${FILE}
 done
+source $HOME/.bash_profile
 cd ../..
-
 ln -s $(realpath dotfiles) ~/.dotfiles
-
 if [[ $(uname -s) == Darwin ]]; then
-    colorEcho "Setting up Mac"
+    colorEcho "Setting up Mac..."
     source setup/install-mac.sh
 elif [[ $(uname -s) == Linux ]]; then
-    colorEcho "Setting up Linux"
+    colorEcho "Setting up Linux..."
     source setup/install-linux.sh
 fi
